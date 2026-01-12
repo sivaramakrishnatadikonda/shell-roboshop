23ea9f0dff1a97912240541131211a556b452116
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-12 20:29:36 +0530

    install nginx and mysql

--- roboshop.sh
+++ roboshop.sh
@@ -7,8 +7,8 @@ DOMAIN_NAME="tadikondadevops.site"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2. micro --security-group-ids sg-0ffc883b3a356e316 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].
-    InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0ffc883b3a356e316 --tag-specifications "ResourceType=instance,
+    Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID   --query "Reservations[0].
