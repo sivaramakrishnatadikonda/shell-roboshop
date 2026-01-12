cb68fe7e6d92889f61666581362ad403ae64f7e6
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-12 20:18:16 +0530

    install nginx and mysql

--- roboshop.sh
+++ roboshop.sh
@@ -12,14 +12,14 @@ do
     micro --security-group-ids sg-0ffc883b3a356e316 --tag-specifications 
     "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
-if [ $instance -ne "frontend" ]
+    if [ $instance -ne "frontend" ]
 
-then
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID   --query
-    "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-else
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID   --query
-    "Reservations[0].Instances[0].PublicIpAddress" --output text)
-fi
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID   --query
+        "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID   --query
+        "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
     echo "$instance IP Address: $IP"
 done
