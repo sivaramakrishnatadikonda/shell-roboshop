65ff80631e4b63ffd01a1a003ddf66e2432a688b
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-12 20:00:05 +0530

    install nginx and mysql

--- roboshop.sh
+++ roboshop.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0ffc883b3a356e316"
-INSTANCES=("mongodb" "catalogue" "forntend")
+INSTANCES=("mongodb")
 ZONE_ID="Z092734529C8LBQP3M7WP"
 DOMAIN_NAME="tadikondadevops.site"
 
 for instance in ${INSTANCES[@]}
 do
 
-aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro 
+INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro 
 --security-group-ids sg-0ffc883b3a356e316 --tag-specifications "ResourceType=instance,
-Tags=[{key=Name, Value=test}]" --query "Instances[0].InstanceId" --output text
+Tags=[{key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
 if [ $instance -ne frontend ]
-aws
 
+then
+   IP=(aws ec2 descrie-instances --instance-ids $INSTANCE_ID   --query
+    "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+else
+   IP=(aws ec2 descrie-instances --instance-ids $INSTANCE_ID   --query
+    "Reservations[0].Instances[0].PublicIpAddress" --output text)
+fi
+    echo "$instance IP Address: $IP"
 done
