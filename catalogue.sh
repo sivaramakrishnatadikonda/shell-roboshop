a0129aa239397a8e8da7fe323dc2126e39ac7173
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-13 20:19:22 +0530

    nginx server

--- catalogue.sh
+++ catalogue.sh
@@ -76,7 +76,7 @@ VALIDATE $? "starting catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-mongosh -y &>>$LOG_FILE
+dnf install mongodb-mongosh -y 
 VALIDATE $? "Installing mongodb client"
 
 #Data base is exit or not to check mongodb
