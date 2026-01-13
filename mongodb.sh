a5237f37a9ba75badfca02819627d94c742a09af
sivaramakrishnatadikonda <sivaramakrishnatadikonda123@gmail.com>
2026-01-13 08:53:23 +0530

    mongodb installation

--- mongodb.sh
+++ mongodb.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-USERID=$(id-u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG-FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at : $(date)" | tee -a $LOG_FILE
 # check the user has root priveleges or not 
-if [ $USERID -ne 0 ]
+if [ $USERID != 0 ]
 then    
     echo -e "$R ERROR:: please run the script with root access $N" | tee -a $LOG_FILE
     exit 1 # give other than 0 upto 127
